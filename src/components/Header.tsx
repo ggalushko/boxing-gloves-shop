1eddb5ceaac666cb8d3d3188b53bb117ff8077f7
ggalushko <titanmelchior@gmail.com>
2023-03-30 12:07:20 +0300

    remove cart icon when in cart

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,15 +14,17 @@ function Header() {
       <NavLink to="/shop" className="text-lg font-bold text-white ml-20">
         Products
       </NavLink>
-      <button
-        className="ml-auto mr-8 bg-white rounded-full p-2 relative"
-        onClick={() => setModalIsOpened(!modalIsOpened)}
-      >
-        <img src="../../src/images/cart.png" className="w-10 h-10"></img>
-        <p className="text-xs absolute font-bold bg-blue-700 rounded-full p-2 -bottom-4 -right-2 w-8 text-center">
-          {cartItemsAmount}
-        </p>
-      </button>
+      {location.pathname === "/cart" ? null : (
+        <button
+          className="ml-auto mr-8 bg-white rounded-full p-2 relative hover:opacity-80 transition-opacity duration-300"
+          onClick={() => setModalIsOpened(!modalIsOpened)}
+        >
+          <img src="../../src/images/cart.png" className="w-10 h-10"></img>
+          <p className="text-xs absolute font-bold bg-blue-700 rounded-full p-2 -bottom-4 -right-2 w-8 text-center">
+            {cartItemsAmount}
+          </p>
+        </button>
+      )}
       {modalIsOpened ? <CartModal></CartModal> : null}
     </header>
   );
