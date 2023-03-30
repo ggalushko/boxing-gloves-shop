7e163cc7968bc823586498f48e264e8a9c8e2c32
ggalushko <titanmelchior@gmail.com>
2023-03-30 12:37:36 +0300

    add message element for empty cart

--- src/components/CartCheckout.tsx
+++ src/components/CartCheckout.tsx
@@ -1,12 +1,16 @@
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 import { CartContext } from "./CartProvider";
 
 function CartCheckout() {
   const cartContext = useContext(CartContext);
   const itemsAmount = cartContext.cartState.totalItems;
   const price = cartContext.cartState.totalPrice;
-  return (
-    <div className="flex flex-col place-content-center w-64 border h-80 bg-white text-black text-2xl rounded-xl gap-20 pb-7 box-content">
+  return itemsAmount > 0 ? (
+    <div
+      className="flex flex-col place-content-center w-64 border h-80
+     bg-white text-black text-2xl rounded-xl gap-20 pb-7 box-content"
+    >
       <p className=" mt-10">Items: {itemsAmount}</p>
       <p>
         Total price: <br />
@@ -16,7 +20,19 @@ function CartCheckout() {
         Buy
       </button>
     </div>
+  ) : (
+    <div className="flex flex-col place-content-center bg-white text-black text-2xl rounded-xl gap-20 pb-7 box-content p-10">
+      <p className="leading-[80px]">
+        Your cart is empty <br /> Visit
+        <Link to="/shop">
+          <span className=" text-blue-600 cursor-pointer hover:opacity-75 transition-opacity duration-300">
+            {" "}
+            products{" "}
+          </span>
+        </Link>
+        page to add a pair of good boxing gloves
+      </p>
+    </div>
   );
 }
-
 export default CartCheckout;
