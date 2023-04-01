2dc3ef54a032c1bacdca2a3872746ea9e00d4735
ggalushko <titanmelchior@gmail.com>
2023-04-01 13:15:30 +0300

    add more initial data

--- src/data/products.ts
+++ src/data/products.ts
@@ -7,4 +7,6 @@ export const products = [
   { id: 5, name: "Grant", price: 1999.99 },
   { id: 6, name: "Venum", price: 199.99 },
   { id: 7, name: "Title", price: 89.99 },
+  { id: 8, name: "Leone", price: 129.99 },
+  { id: 9, name: "Rival", price: 139.99 },
 ];
